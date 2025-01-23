ff5a68f15e26c1bad4dbb991518c785634106e95
Ravi Rafaliya <ravirr6997@gmail.com>
2025-01-23 17:12:16 +0530

    add functinality

--- src/assets/Components/Hero.jsx
+++ src/assets/Components/Hero.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import heroImg from "../Images/videochat.png";
 
 const Hero = () => {
+  const [input, setInput] = React.useState("");
+
+  const submitHandler = () => {
+    if (input) {
+      window.location.href = `/room/${input}`;
+    }
+  };
+
   return (
     <div className="bg-gray-50">
       <section className="pt-12 pb-12 sm:pb-16 lg:pt-8">
@@ -20,16 +28,18 @@ const Hero = () => {
                 <div className="mt-8 sm:mt-10">
                   <div className="relative p-2 sm:border sm:border-gray-400 group sm:rounded-xl sm:focus-within:ring-1 sm:focus-within:ring-gray-900 sm:focus-within:border-gray-900">
                     <input
-                      type="email"
+                      type="text"
+                      value={input}
+                      onChange={(e) => setInput(e.target.value)}
                       name=""
                       id=""
-                      placeholder="Enter email address"
+                      placeholder="Enter your Room ID"
                       className="block w-full px-4 py-4 text-gray-900 placeholder-gray-900 bg-transparent border border-gray-400 outline-none focus:border-gray-900 focus:ring-1 focus:ring-gray-900 rounded-xl sm:border-none sm:focus:ring-0 sm:focus:border-transparent"
                       required=""
                     />
                     <div className="mt-4 sm:mt-0 sm:absolute sm:inset-y-0 sm:right-0 sm:flex sm:items-center sm:pr-2">
                       <button
-                        type="submit"
+                        onClick={submitHandler}
                         className="inline-flex px-6 py-3 text-lg font-bold text-white transition-all duration-200 bg-pink-500 rounded-lg focus:outline-none focus:bg-gray-600 font-pj hover:bg-gray-600"
                       >
                         Join Us Now
--- src/assets/Components/VideoComp.jsx
+++ src/assets/Components/VideoComp.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import { useParams } from "react-router-dom";
 
 const VideoComp = () => {
-  return <div>VideoComp</div>;
+  const { roomID } = useParams();
+  console.log(roomID);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    let myMeeting = async (element) => {
+      if (!roomID || !containerRef.current) return;
+      // generate Kit Token
+      const appID = 451483442;
+      const serverSecret = "64178018d8002a972024c4b0e0f282f9";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appID,
+        serverSecret,
+        roomID,
+        Date.now().toString(),
+        "Anonymous"
+      );
+
+      // Create instance object from Kit Token.
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      // start the call
+      zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: "Personal link",
+            url:
+              window.location.protocol +
+              "//" +
+              window.location.host +
+              window.location.pathname +
+              "?roomID=" +
+              roomID,
+          },
+        ],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+        },
+      });
+    };
+
+    myMeeting();
+
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.innerHTML = "";
+      }
+    };
+  }, [roomID]);
+
+  return (
+    <div
+      className="myCallContainer"
+      ref={containerRef}
+      style={{ width: "100vw", height: "100vh" }}
+    ></div>
+  );
 };
 
 export default VideoComp;
